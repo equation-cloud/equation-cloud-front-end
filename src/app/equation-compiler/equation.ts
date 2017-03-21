0385940458ee63fdf3afeba27968b4d8922e6f83
Rob Bacon <bacon@cantab.net>
2017-03-21 21:16:20 +0000

    Provide a decorated TeX string
    Changed the variable list to be more complex
    Modified the equations.component.html to use the decorated TeX string

--- src/app/equation-compiler/equation.ts
+++ src/app/equation-compiler/equation.ts
@@ -1,9 +1,12 @@
-import { IExpression } from './expression-types'
+import { IExpression, OperatorExpression } from './expression-types'
 
 export class Equation {
   public lexerErrors? : any;
   public parserErrors? : any;
   public expressionTree? : IExpression;
+  private rawTeX? : string;
+  private decoratedTeX? : string;
+  private variables : any[] = [];
 
   constructor(lexerErrors? : any, parserErors? : any, expressionTree? : IExpression)
   {
@@ -15,14 +18,57 @@ export class Equation {
     }
     if(!this.lexerErrors && !this.parserErrors) {
       this.expressionTree = expressionTree;
+      this.generateItemLists(this.expressionTree);
+      this.rawTeX = this.expressionTree.getTeX(false);
+      this.decoratedTeX = this.expressionTree.getTeX(true);
+      console.log(this.decoratedTeX);
     }
   }
 
+  //Read only properties
+
   get RawTeX() : string {
-    return this.expressionTree ? this.expressionTree.getRawTeX() : null;
+    return this.rawTeX ? this.rawTeX : null;
   }
 
-  get VariableList() : string[] {
-    return this.expressionTree ? this.expressionTree.getVariableNames() : null;
+  get DecoratedTeX() : string {
+    return this.decoratedTeX ? this.decoratedTeX : null;
+  }
+
+  get Variables() : string[] {
+    return this.variables;
+  }
+
+  //methods
+
+  private generateItemLists(expression: IExpression) : void {
+    //recursivley traverse through the tree and add variables to a list
+    if(expression instanceof OperatorExpression) {
+      for(let operand of expression.operands) {
+        this.generateItemLists(operand);
+      }
+    } else {
+      switch(expression.type){
+        case "variable":
+          let item = this.variables.find((value, index, obj) => {
+            return value.name == expression.value;
+          });
+          if(item) {
+            let id = expression.value + "_" + item.ids.length;
+            item.ids.push(id);
+          } else {
+            let id = expression.value + "_0";
+            expression['id'] = id;
+            this.variables.push({
+              name: expression.value,
+              ids : [id]
+            })
+          }
+          break;
+        default:
+          console.error("Unrecognized expression type: " + expression.type);
+          return;
+      }
+    }
   }
 }
