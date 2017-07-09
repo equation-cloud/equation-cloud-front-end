b10c23b0a0e20967df0e166e7ddb9b43670a9ca5
Hugh Johnson <hugh@hughjohnson.net>
2017-07-09 21:52:03 +0100

    Add a short delay before showing MathJax output and variables, fixes #5

--- src/app/mathjax.directive.ts
+++ src/app/mathjax.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Input, Output } from '@angular/core';
 
 declare var MathJax: any;
 
@@ -8,10 +8,15 @@ declare var MathJax: any;
 export class MathJaxDirective {
   @Input('MathJax') equationString: string;
 
+  @Output() mathReady = new EventEmitter<boolean>();
+
   constructor(private el: ElementRef) { }
 
   ngOnChanges() {
+    this.mathReady.emit(false);
     this.el.nativeElement.innerHTML = '<script type="math/tex">\\displaystyle{' + this.equationString + '}</script>';
     MathJax.Hub.Queue(["Typeset", MathJax.Hub, this.el.nativeElement]);
+    MathJax.Hub.Queue(this.mathReady.emit(true));
   }
+
 }
