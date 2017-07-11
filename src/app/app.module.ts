f1070428374e8daea462a207b969ebf9aa7968fc
Hugh Johnson <hugh@hughjohnson.net>
2017-07-11 08:14:08 +0100

    Add routing to 'app' module and create 'equations' module
    This is to prepare for adding routing and renaming equations component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { MathJaxDirective } from './mathjax.directive';
@@ -10,6 +11,8 @@ import { EquationsComponent } from './equations/equations.component';
 import { AppReadyEventService } from './app-ready-event.service';
 import { ClipboardModule } from 'ngx-clipboard';
 import { FooterComponent } from './footer/footer.component';
+import { EquationsRoutingModule } from './equations/equations-routing.module';
+import { EquationsModule } from './equations/equations.module';
 
 @NgModule({
   declarations: [
@@ -23,11 +26,15 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    ClipboardModule
+    ClipboardModule,
+    EquationsRoutingModule,
+    EquationsModule
   ],
   providers: [
     AppReadyEventService
+    AppRoutingModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/equations/equations.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EquationsRoutingModule } from './equations-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    EquationsRoutingModule
+  ],
+  declarations: []
+})
+export class EquationsModule { }
